5197a1f5a42876bad66fb49ab79b042ee3f06892
chick <chick@qrhino.com>
2016-08-19 16:49:21 -0700

    Looks like we may have actually added 2 blackbox doubles Still to do, get step action going

--- src/main/scala/dsp/real/DspRealBlackBox.scala
+++ src/main/scala/dsp/real/DspRealBlackBox.scala
@@ -2,11 +2,23 @@
 
 package dsp.real
 
-import firrtl_interpreter.{ConcreteUInt, Concrete, BlackBoxImplementation, BlackBoxFactory}
+import firrtl_interpreter._
 
-class DspRealAdd(name: String) extends BlackBoxImplementation {
-  def execute: Concrete = {
-    ConcreteUInt(-1, 1)
+class DspRealAdd(val name: String) extends BlackBoxImplementation {
+  def execute(inputValues: Seq[Concrete]): Concrete = {
+    val arg1 :: arg2 :: tail = inputValues
+    val doubleArg1 = java.lang.Double.longBitsToDouble(arg1.value.toLong)
+    val doubleArg2 = java.lang.Double.longBitsToDouble(arg2.value.toLong)
+    val doubleResult = doubleArg1 + doubleArg2
+    val result = BigInt(java.lang.Double.doubleToLongBits(doubleResult))
+    val long2 = arg2.value.toLong
+    ConcreteUInt(result, 64)
+  }
+  def outputDependencies(outputName: String): Seq[(String)] = {
+    outputName match {
+      case "out" => Seq(fullName("in1"), fullName("in2"))
+      case _ => Seq.empty
+    }
   }
 }
 
@@ -14,13 +26,13 @@ class DspRealFactory extends BlackBoxFactory {
   def appliesTo(blackBoxName: String): Boolean = {
     blackBoxName match {
       case "BBFAdd" => true
-      case _ => false
+      case _        => false
     }
   }
-  def constructor(info: String): BlackBoxImplementation = {
-    info match {
-      case "Add" =>
-        new DspRealAdd("RealAdder")
+  def createInstance(instanceName: String, blackBoxName: String): Option[BlackBoxImplementation] = {
+    blackBoxName match {
+      case "BBFAdd" => Some(add(new DspRealAdd(instanceName)))
+      case _        => None
     }
   }
 }
--- src/test/scala/dsp/real/FactorySpec.scala
+++ src/test/scala/dsp/real/FactorySpec.scala
@@ -14,6 +14,14 @@ class FactorySpec extends FlatSpec with Matchers {
     val input = io.Source.fromFile("src/test/resources/RealAdder.fir").mkString
 
     val tester = new InterpretiveTester(input, blackBoxFactories = Seq(new DspRealFactory))
+    tester.interpreter.verbose = true
+    tester.interpreter.setVerbose(true)
+
+    tester.poke("io_a1_node", 100)
+    tester.poke("io_a2_node", 200)
+    tester.step()
+
+    println(s"output is ${tester.peek("io_c_node")}")
 
     tester.interpreter.dependencyGraph.outputPorts.size should be > 0
   }
