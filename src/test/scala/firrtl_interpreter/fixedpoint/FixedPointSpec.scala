e2339fb1847f7122648edc75f5e3da25914adfd3
chick <chick@qrhino.com>
2016-09-12 12:57:09 -0700

    add test for set binary point with fixedtype

--- src/test/scala/firrtl_interpreter/fixedpoint/FixedPointSpec.scala
+++ src/test/scala/firrtl_interpreter/fixedpoint/FixedPointSpec.scala
@@ -28,4 +28,63 @@ class FixedPointSpec extends FlatSpec with Matchers {
 
     tester.expect("c", BigInt("1000", 2))
   }
+
+  behavior of "allow zero length binary point"
+
+  it should "be happy with zero" in {
+//    val input =
+//      """
+//        |circuit SBP :
+//        |  module SBP :
+//        |    input clk : Clock
+//        |    input reset : UInt<1>
+//        |    output io : {flip in : Fixed<6><<2>>, out : Fixed}
+//        |
+//        |    io is invalid
+//        |    io.out <= io.in
+//      """.stripMargin
+    val input =
+      """
+        |circuit Unit :
+        |  module Unit :
+        |    input clk : Clock
+        |    input reset : UInt<1>
+        |    input io_in : Fixed<6><<0>>
+        |    output io_out : Fixed
+        |
+        |    io_in is invalid
+        |    io_out is invalid
+        |    io_out <= io_in
+      """.stripMargin
+    val tester = new InterpretiveTester(input)
+    tester.interpreter.verbose = true
+    tester.interpreter.setVerbose(true)
+
+
+    tester.poke("io_in", BigInt("11", 2))
+    println(s"got ${tester.peek("io_out")}")
+  }
+  behavior of "set binary point"
+
+  it should "shorten number with new binary point" in {
+    val input =
+      """
+        |circuit SBP :
+        |  module SBP :
+        |    input clk : Clock
+        |    input reset : UInt<1>
+        |    output io : {flip in : Fixed<6><<2>>, out : Fixed}
+        |
+        |    io is invalid
+        |    node T_2 = bpset(io.in, 0)
+        |    io.out <= T_2
+      """.stripMargin
+    val tester = new InterpretiveTester(input)
+    tester.interpreter.verbose = true
+    tester.interpreter.setVerbose(true)
+
+
+    tester.poke("io_in", BigInt("1011", 2))
+    println(s"got ${tester.peek("io_out")}")
+  }
 }
