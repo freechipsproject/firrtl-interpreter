54f2821c7edcca725235ac98f0e5afea651bfe2c
jackkoenig <jack.koenig3@eecs.berkeley.edu>
2016-11-07 16:20:35 -0800

    Adhere to new Firrtl Compiler API

--- src/main/scala/firrtl_interpreter/ToLoFirrtl.scala
+++ src/main/scala/firrtl_interpreter/ToLoFirrtl.scala
@@ -6,13 +6,14 @@ import java.io.{StringWriter, Writer}
 
 import firrtl.LowFirrtlCompiler
 import firrtl.ir.Circuit
+import firrtl.ChirrtlForm
 import firrtl.Annotations.AnnotationMap
 
 object ToLoFirrtl {
   def lower(c: Circuit): Circuit = {
     val compiler = new LowFirrtlCompiler
 
-    val compileResult = compiler.compile(c, new AnnotationMap(Seq.empty), new StringWriter())
+    val compileResult = compiler.compile(firrtl.CircuitState(c, ChirrtlForm), new StringWriter)
     compileResult.circuit
   }
 }
